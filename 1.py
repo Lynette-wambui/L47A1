20f2eab0390d40a0b6611ca679a90cb33b6d5356
LYNETTE WAMBUI <lynettemuraga47@gmail.com>
2025-05-18 18:26:37 +0300

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,10 @@
+class student:
+    name = "Lynette"
+    print("Hi I am called ", name)
+    grade = 10
+    print("I am in Grade", grade)
+    age = 13
+    print("I am of age", age)
+    percentage = 90
+    print("I get percentage", percentage)
+ob = student()
